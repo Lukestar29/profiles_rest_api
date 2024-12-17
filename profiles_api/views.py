566faf9d2aeee91f91acbf1fb94c4ece932871b1
Luke <lukesweeneyletterkenny@gmail.com>
2024-12-17 09:58:23 +0000

    added functions to the viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -7,6 +7,7 @@ from profiles_api import serializers
 
 class HelloViewSet(viewsets.ViewSet):
     """Test API Viewset"""
+    serializer_class = serializers.HelloSerializer
 
     def list(self, request):
         """Return a hello message"""
@@ -18,6 +19,36 @@ class HelloViewSet(viewsets.ViewSet):
         ]
     
         return Response({"message": a_viewset})
+    
+    def create(self, request):
+        """Create a new hello message"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get("name")
+            message = f"Hello {name}"
+            return Response({"message": message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+    
+    def retrieve(self, request, pk=None):
+        """Handle getting an object by its ID"""
+        return Response({"http_method": "GET"})
+    
+    def update(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({"http_method": "PUT"})
+    
+    def partial_update(self, request, pk=None):
+        """Handle updating part of an object"""
+        return Response({"http_method": "PATCH"})
+    
+    def destroy(self, request, pk=None):
+        """Handle removing an object"""
+        return Response({"http_method": "DELETE"})
 
 class HelloApiView(APIView):
     """Test API View"""
